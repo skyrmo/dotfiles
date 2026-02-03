51ea2364cf0f9c5b4fbc712f0211ab7d9fde0e9c
Skyrmo <owaingskyrme@gmail.com>
2026-02-03 19:54:28 +0800

    Updated Zed settings.

--- runs/02_ohmyzsh.sh
+++ runs/02_ohmyzsh.sh
@@ -5,7 +5,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-# Create .zshrc configuration
+
 # Create .zshrc configuration (overwrite)
 cat > ~/.zshrc << 'EOL'
 # ---------- PATH (base) ----------
